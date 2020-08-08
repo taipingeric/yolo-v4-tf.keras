ca2a3779acc4c2103ff1f47a2152204bc1d30be0
EricLi <taipingeric@gmail.com>
2020-08-08 10:55:36 +0800

    add random seed

--- utils.py
+++ utils.py
@@ -76,11 +76,11 @@ def get_detection_data(img, model_outputs, class_names):
     print(f'# of bboxes: {num_bboxes}')
     return df
 
-def read_annotation_lines(annotation_path, test_size=None):
+def read_annotation_lines(annotation_path, test_size=None, random_seed=5566):
     with open(annotation_path) as f:
         lines = f.readlines()
     if test_size:
-        return train_test_split(lines, test_size=test_size)
+        return train_test_split(lines, test_size=test_size, random_state=random_seed)
     else:
         return lines
 
