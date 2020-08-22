e93a29dd5fb18f99c3ae145aed0064dd8c3f4128
EricLi <taipingeric@gmail.com>
2020-08-22 14:05:07 +0800

    update

--- models.py
+++ models.py
@@ -96,7 +96,7 @@ class Yolov4(object):
                                 epochs=epochs,
                                 callbacks=callbacks,
                                 initial_epoch=initial_epoch)
-    # raw_img: channel last
+    # raw_img: RGB
     def predict_img(self, raw_img, random_color=True, plot_img=True, figsize=(10, 10), show_text=True):
         print('img shape: ', raw_img.shape)
         img = self.preprocess_img(raw_img)
@@ -111,7 +111,7 @@ class Yolov4(object):
         return detections
 
     def predict(self, img_path, random_color=True, plot_img=True, figsize=(10, 10), show_text=True):
-        raw_img = cv2.imread(img_path)[:,:,::-1]
+        raw_img = cv2.imread(img_path)[:, :, ::-1]
         return self.predict_img(raw_img, random_color, plot_img, figsize, show_text)
 
     def export_gt(self, annotation_path, gt_folder_path):
