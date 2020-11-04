beb029013f143de33f3205b7ad12bae4e803916a
EricLi <taipingeric@gmail.com>
2020-11-04 16:50:28 +0800

    update comments

--- loss.py
+++ loss.py
@@ -62,57 +62,48 @@ def bbox_giou(boxes1, boxes2):
 
 def bbox_ciou(boxes1, boxes2):
     '''
-    计算ciou = iou - p2/c2 - av
+    ciou = iou - p2/c2 - av
     :param boxes1: (8, 13, 13, 3, 4)   pred_xywh
     :param boxes2: (8, 13, 13, 3, 4)   label_xywh
     :return:
-
-    举例时假设pred_xywh和label_xywh的shape都是(1, 4)
     '''
-
-    # 变成左上角坐标、右下角坐标
     boxes1_x0y0x1y1 = tf.concat([boxes1[..., :2] - boxes1[..., 2:] * 0.5,
                                  boxes1[..., :2] + boxes1[..., 2:] * 0.5], axis=-1)
     boxes2_x0y0x1y1 = tf.concat([boxes2[..., :2] - boxes2[..., 2:] * 0.5,
                                  boxes2[..., :2] + boxes2[..., 2:] * 0.5], axis=-1)
-    '''
-    逐个位置比较boxes1_x0y0x1y1[..., :2]和boxes1_x0y0x1y1[..., 2:]，即逐个位置比较[x0, y0]和[x1, y1]，小的留下。
-    比如留下了[x0, y0]
-    这一步是为了避免一开始w h 是负数，导致x0y0成了右下角坐标，x1y1成了左上角坐标。
-    '''
     boxes1_x0y0x1y1 = tf.concat([tf.minimum(boxes1_x0y0x1y1[..., :2], boxes1_x0y0x1y1[..., 2:]),
                                  tf.maximum(boxes1_x0y0x1y1[..., :2], boxes1_x0y0x1y1[..., 2:])], axis=-1)
     boxes2_x0y0x1y1 = tf.concat([tf.minimum(boxes2_x0y0x1y1[..., :2], boxes2_x0y0x1y1[..., 2:]),
                                  tf.maximum(boxes2_x0y0x1y1[..., :2], boxes2_x0y0x1y1[..., 2:])], axis=-1)
 
-    # 两个矩形的面积
+    # area
     boxes1_area = (boxes1_x0y0x1y1[..., 2] - boxes1_x0y0x1y1[..., 0]) * (
                 boxes1_x0y0x1y1[..., 3] - boxes1_x0y0x1y1[..., 1])
     boxes2_area = (boxes2_x0y0x1y1[..., 2] - boxes2_x0y0x1y1[..., 0]) * (
                 boxes2_x0y0x1y1[..., 3] - boxes2_x0y0x1y1[..., 1])
 
-    # 相交矩形的左上角坐标、右下角坐标，shape 都是 (8, 13, 13, 3, 2)
+    # top-left and bottom-right coord, shape: (8, 13, 13, 3, 2)
     left_up = tf.maximum(boxes1_x0y0x1y1[..., :2], boxes2_x0y0x1y1[..., :2])
     right_down = tf.minimum(boxes1_x0y0x1y1[..., 2:], boxes2_x0y0x1y1[..., 2:])
 
-    # 相交矩形的面积inter_area。iou
+    # intersection area and iou
     inter_section = tf.maximum(right_down - left_up, 0.0)
     inter_area = inter_section[..., 0] * inter_section[..., 1]
     union_area = boxes1_area + boxes2_area - inter_area
     iou = inter_area / (union_area + 1e-9)
 
-    # 包围矩形的左上角坐标、右下角坐标，shape 都是 (8, 13, 13, 3, 2)
+    # top-left and bottom-right coord of the enclosing rectangle, shape: (8, 13, 13, 3, 2)
     enclose_left_up = tf.minimum(boxes1_x0y0x1y1[..., :2], boxes2_x0y0x1y1[..., :2])
     enclose_right_down = tf.maximum(boxes1_x0y0x1y1[..., 2:], boxes2_x0y0x1y1[..., 2:])
 
-    # 包围矩形的对角线的平方
+    # diagnal ** 2
     enclose_wh = enclose_right_down - enclose_left_up
     enclose_c2 = K.pow(enclose_wh[..., 0], 2) + K.pow(enclose_wh[..., 1], 2)
 
-    # 两矩形中心点距离的平方
+    # center distances between two rectangles
     p2 = K.pow(boxes1[..., 0] - boxes2[..., 0], 2) + K.pow(boxes1[..., 1] - boxes2[..., 1], 2)
 
-    # 增加av。加上除0保护防止nan。
+    # add av
     atan1 = tf.atan(boxes1[..., 2] / (boxes1[..., 3] + 1e-9))
     atan2 = tf.atan(boxes2[..., 2] / (boxes2[..., 3] + 1e-9))
     v = 4.0 * K.pow(atan1 - atan2, 2) / (math.pi ** 2)
